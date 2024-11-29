0b0490faf442821a19b000c8cb4c59ceda73ba89
kashish <you@example.com>
2024-11-29 22:22:34 +0530

    third commit

--- auth_app/models.py
+++ auth_app/models.py
@@ -6,27 +6,47 @@ import random
 from django.utils.timezone import now
 from django.contrib.auth import get_user_model
 
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, phone_number, email=None, password=None, **extra_fields):
+#         if not phone_number:
+#             raise ValueError("The phone number is required")
+#         email = self.normalize_email(email)
+#         extra_fields.setdefault('is_active', True)  
+#         user = self.model(phone_number=phone_number, email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+
 class CustomUserManager(BaseUserManager):
-    def create_user(self, phone_number, email=None, password=None, **extra_fields):
-        if not phone_number:
-            raise ValueError("The phone number is required")
-        email = self.normalize_email(email)
-        extra_fields.setdefault('is_active', True)  
-        user = self.model(phone_number=phone_number, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+  def create_user(self,phone_number,password=None,**extra_fields):
+    if not phone_number:
+      raise ValueError("phone number is required")
+    user=self.model(phone_number=phone_number,**extra_fields)
+    user.set_password(password)
+    user.save(using=self.db)
+    return user
+        
+  def create_superuser(self,phone_number,password=None,**extra_fields):
+    extra_fields.setdefault('is_staff',True)
+    extra_fields.setdefault('is_superuser',True)
+    extra_fields.setdefault('is_active',True)
+    return self.create_user(phone_number,password,**extra_fields)
 
-    def create_superuser(self, phone_number, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    
 
-        if not extra_fields.get('is_staff'):
-            raise ValueError('Superuser must have is_staff=True.')
-        if not extra_fields.get('is_superuser'):
-            raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(phone_number, password=password, **extra_fields)
+    # def create_superuser(self, phone_number, password, **extra_fields):
+    #     extra_fields.setdefault('is_staff', True)
+    #     extra_fields.setdefault('is_superuser', True)
+
+    #     if not extra_fields.get('is_staff'):
+    #         raise ValueError('Superuser must have is_staff=True.')
+    #     if not extra_fields.get('is_superuser'):
+    #         raise ValueError('Superuser must have is_superuser=True.')
+
+    #     return self.create_user(phone_number, password=password, **extra_fields)
     
         
 
