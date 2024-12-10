9abf1cd279044b7e9d6fc31fc8f7f84c6952265a
kashish <you@example.com>
2024-12-10 12:44:02 +0530

    industry

--- products/models.py
+++ products/models.py
@@ -3,7 +3,6 @@ from services.models import Services
 
 class Products(models.Model):
     title = models.CharField(max_length=100)
-    industry_type = models.CharField(max_length=100)
     orientation = models.CharField(max_length=200,choices=[('horizontal','Horizontal'),('vertical','Vertical')],default='horizontal')
     material_type = models.CharField(max_length=100,default="abc")
     service = models.ForeignKey(Services,on_delete=models.CASCADE)
