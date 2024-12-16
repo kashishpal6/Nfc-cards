2ba70b9cdf5fa0814054792976145a8cd878e280
kashish <you@example.com>
2024-12-16 16:35:22 +0530

    userrrrr

--- return_page/models.py
+++ return_page/models.py
@@ -3,7 +3,7 @@ from purchase.models import Purchase
 from auth_app.models import CustomUser
 class ReturnPage(models.Model):
     purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name="pages")  
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name ="customer",default='None')
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name ="customer",default='00')
     productCode = models.CharField(max_length=100,default='default')
     image = models.ImageField(upload_to='returns/')
     reason = models.TextField()
