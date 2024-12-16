a856e57211e0f7693809adf11cc6b66f3255cac3
kashish <you@example.com>
2024-12-16 16:34:19 +0530

    resolve

--- return_page/models.py
+++ return_page/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from purchase.models import Purchase
-
+from auth_app.models import CustomUser
 class ReturnPage(models.Model):
     purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name="pages")  
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name ="customer",default='None')
     productCode = models.CharField(max_length=100,default='default')
     image = models.ImageField(upload_to='returns/')
     reason = models.TextField()
--- return_page/serializers.py
+++ return_page/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import ReturnPage
 
 class ReturnPageSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
        model = ReturnPage
-       fields = ['purchase_id','productCode','reason','image','request_type','quantity']
+       fields = ['purchase_id','productCode','reason','image','request_type','quantity','user']
