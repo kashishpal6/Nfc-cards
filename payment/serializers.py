92e9a9f738c96e34cb8dbbe81b72f31db7cfc47d
kashish <you@example.com>
2024-12-16 12:57:16 +0530

    payment_id

--- payment/serializers.py
+++ payment/serializers.py
@@ -5,4 +5,4 @@ class paymentSerailizer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
        model = Payment
-       fields = '__all__'
+       fields = ['payment_id','price','status','payment_method','product_variation','UTR' ]
