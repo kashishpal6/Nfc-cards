d828a6e375492a3bb1b17918e581f836ca3b5daf
kashish <you@example.com>
2024-12-16 12:35:58 +0530

    retrievePurchase

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from purchase.views import createPurchase,listpurchase
+from purchase.views import createPurchase,listpurchase,Retrievepurchase
 
 
 urlpatterns = [
    
     path('createPurchase/',createPurchase.as_view(),name="create"),
-    path('listPurchase/',listpurchase.as_view(),name="list")
+    path('listPurchase/',listpurchase.as_view(),name="list"),
+    path('Retrievepurchase/',Retrievepurchase.as_view(),name="Retrievepurchase")
  
 ]
