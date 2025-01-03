0ff4f536ebe2c169999c1ea57e6cd5bd708112b2
kashish <you@example.com>
2025-01-04 00:36:22 +0530

    looginn

--- project/settings.py
+++ project/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kashish.palkurmato@gmail.com' 
-EMAIL_HOST_PASSWORD = 'cypbmchzyxqogmds'
+EMAIL_HOST_PASSWORD = 'inzletozbvojlabs'
 
 
 # Using database-backed sessions (default)
