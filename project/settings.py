fdbd6c9089cdddfc4d88e797176f454008b2fd20
kashish <you@example.com>
2025-01-06 16:21:54 +0530

    cart_changesss

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'return_page',
     'banner',
     'rest_framework_simplejwt',
-    'django_filters',
+    
 
 ]
     
@@ -164,7 +164,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
