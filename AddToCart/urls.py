19ae8c205d49131e690a4e78251a10efb832f9ef
kashish <you@example.com>
2025-01-09 09:52:41 +0530

    cartlist

--- AddToCart/urls.py
+++ AddToCart/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    
     path('createCart/',createCart.as_view(),name="create"),
     path('listCart/',listCart.as_view(),name="list"),
-    path('RetrieveCart/',RetrieveCart.as_view(),name="Retrieve"),
-    path('UpdateCart',UpdateCart.as_view(),name="Update"),
-    path('DestroyCart',DestroyCart.as_view(),name="Destroy")
+    path('RetrieveCart/<int:pk>/',RetrieveCart.as_view(),name="Retrieve"),
+    path('UpdateCart/<int:pk>/',UpdateCart.as_view(),name="Update"),
+    path('DestroyCart/<int:pk>/',DestroyCart.as_view(),name="Destroy")
 ]
