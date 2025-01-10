251bb0821d92da6a762ef0d13f844da30533ae63
kashish <you@example.com>
2025-01-10 13:16:13 +0530

    authtokenn

--- AddToCart/serializers.py
+++ AddToCart/serializers.py
@@ -8,22 +8,36 @@ class AddToCartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = ['user','id','variant','front_data','back_data']
 
+from rest_framework import serializers
+from .models import Cart
+from variant.models import variant  # Make sure you're importing the correct `variant` model
+
 class GetCartSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     variant_name = serializers.SerializerMethodField()
-    variant_image = serializers.SerializerMethodField()
+    variant_color = serializers.SerializerMethodField()
     variant_price = serializers.SerializerMethodField()
+    variant_front_image = serializers.SerializerMethodField()
+    variant_back_image = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
-        fields = ['user', 'id', 'variant', 'variant_name', 'variant_image', 'variant_price', 'front_data', 'back_data']
+        fields = ['user', 'id', 'variant', 'variant_name', 'variant_color', 'variant_price', 'variant_front_image','variant_back_image','front_data', 'back_data']
 
     def get_variant_name(self, obj):
-        return obj.variant.name if obj.variant else None
+        # Retrieve the name of the related variant (assuming `variant.name` exists)
+        return obj.variant.product.name if obj.variant else None
 
-    def get_variant_image(self, obj):
-        return obj.variant.image.url if obj.variant and obj.variant.image else None
+    def get_variant_color(self, obj):
+        # Retrieve the color from the related variant
+        return obj.variant.color if obj.variant else None
 
     def get_variant_price(self, obj):
+        # Retrieve the price from the related variant
         return obj.variant.price if obj.variant else None
+
+    def get_variant_front_image(self, obj):
+        # Retrieve the front image URL from the related variant
+        return obj.variant.front_image.url if obj.variant and obj.variant.front_image else None
+
     
