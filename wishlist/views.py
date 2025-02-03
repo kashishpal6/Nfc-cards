1704994dcbecfd1a923be228c5c59847a6117d08
kashishpal6 <kashishpal386@gmail.com>
2025-02-03 10:28:10 +0530

    commit--b

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,6 +2,8 @@ from .serializers import WishlistSerializer
 from .models import Wishlist
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 class createWishlist(generics.CreateAPIView):
    queryset=Wishlist.objects.all()
@@ -10,7 +12,13 @@ class createWishlist(generics.CreateAPIView):
 
    def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
+   def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "Product added to wishlist successfully."},
+            status=status.HTTP_201_CREATED
+        )
+   
 class listWishlist(generics.ListAPIView):
    queryset=Wishlist.objects.all()
    serializer_class=WishlistSerializer
@@ -25,14 +33,28 @@ class RetrieveWishlist(generics.RetrieveAPIView):
    permission_classes= [IsAuthenticated]
 
 class UpdateWishlist(generics.UpdateAPIView):
-    queryset=Wishlist.objects.all()
-    serializer_class=WishlistSerializer
-    permission_classes= [IsAuthenticated]
+   queryset=Wishlist.objects.all()
+   serializer_class=WishlistSerializer
+   permission_classes= [IsAuthenticated]
 
-    def perform_update(self, serializer):
+   def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+
+   def update(self, request, *args, **kwargs):
+       response = super().update(request, *args, **kwargs)
+       return Response(
+           {"message":"Product updated successfully."},
+           status=status.HTTP_200_OK
+       )
 
 class DestroyWishlist(generics.DestroyAPIView):
    queryset=Wishlist.objects.all()
    serializer_class=WishlistSerializer
    permission_classes= [IsAuthenticated]
+   def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+
+        return Response(
+            {"message": "Product removed from wishlist successfully."},
+            status=status.HTTP_200_OK
+        )
