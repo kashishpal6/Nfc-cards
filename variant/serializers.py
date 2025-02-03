cf6c87a5f3e5e643c297ffe9b39097e840ee80ae
kashishpal6 <kashishpal386@gmail.com>
2025-02-03 17:46:41 +0530

    serializer update

--- variant/serializers.py
+++ variant/serializers.py
@@ -3,8 +3,9 @@ from .models import variant
 
 
 class variantSerializer(serializers.ModelSerializer):
-   product_id = serializers.IntegerField(source = "id",read_only = True)
+   variant_id = serializers.IntegerField(source = "id",read_only = True)
+   product_id = serializers.IntegerField(source = "product.id",read_only = True)
 
    class Meta:
       model=variant
-      fields='__all__'
+      fields=['variant_id','product_id','color','stock','price','selling_price','front_image','back_image','material_type','shape','orientation']
